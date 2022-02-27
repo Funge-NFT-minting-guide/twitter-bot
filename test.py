9eb5a2ec084ce38a4a54ce2475e11124cd59fe63
vulpes0zedra <vulpes0zedra@gmail.com>
2022-02-27 16:52:15 +0900

    implementing get_tweet_full_content(). parsing its elements by xpath?

--- test.py
+++ test.py
@@ -1,39 +1,9 @@
-import json
+import importlib
 
-import twitter
-from env.keys_and_tokens import *
-
-
-twitter_api = twitter.Api(consumer_key=api_key, consumer_secret=api_secret, access_token_key=access_token, access_token_secret=access_secret)
+from twitty import *
+#importlib.reload(twitty)
 
 
-'''
-account = '@meta_kongz'
-print(twitter_api.GetUserTimeline(screen_name=account, count=100, include_rts=False, exclude_replies=False))
-'''
+bot = TwitterBot('test')
 
-query = '민팅 klay -nftart -filter:links -filter:replies -filter:retweets'
-#keyword_necesary = ['민팅']
-tweets = twitter_api.GetSearch(term=query, count=10000)
-#[print(tweets[i].text+'\n'+'-'*60) for i in range(len(tweets))]
-for tweet in tweets:
-    print(f'id: {tweet.id}')
-    print(f'Project: {tweet.user.name}')
-    print(f'Agenda: {tweet.text}')
-    print('-'*40)
-
-
-'''
-query = ['nft', 'minting']
-
-stream = twitter_api.GetStreamFilter(track=query)
-while True:
-    for tweets in stream:
-        try:
-            print(tweets['text'])
-            print('-------------------------------------')
-
-        except Exception as e:
-            print(e)
-            break
-'''
+bot.search_tweets('test', '민팅 klay -nftart -filter:links -filter:replies -filter:retweets')
---
+++ twitty.py
@@ -0,0 +1,100 @@
+import re
+import json
+import datetime
+import requests
+from copy import copy
+from  pprint import PrettyPrinter
+
+import twitter
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+from env.db_config import *
+from env.keys_and_tokens import *
+
+
+class TwitterBot:
+    def __init__(self, db_name):
+        self.db_host = copy(db_config['host'])
+        self.db_port = copy(db_config['port'])
+        self.db_username = copy(db_config['username'])
+        self.db_password = copy(db_config['password'])
+        self.db_name = db_name
+        self.api_key = copy(twitter_config['api_key'])
+        self.api_secret = copy(twitter_config['api_secret'])
+        self.access_token = copy(twitter_config['access_token'])
+        self.access_secret = copy(twitter_config['access_secret'])
+        self.mongo_client = self.get_connect()
+        self.twitter_api = self.get_twitter_api()
+        self.re_tweet_content_url = re.compile(r'(https:\/\/t\.co\/\w{10})$')
+        self.pprinter = PrettyPrinter()
+
+
+    def get_connect(self):
+        return MongoClient(host=self.db_host, port=0, username=self.db_username, password=self.db_password)
+
+
+    def get_twitter_api(self):
+        return twitter.Api(consumer_key=self.api_key, consumer_secret=self.api_secret, access_token_key=self.access_token, access_token_secret=self.access_secret)
+
+
+    def get_tweet_full_content(self, text):
+        url = self.re_tweet_content_url.findall(text)[0]
+        return url
+        r = requests.get(url)
+        if r.status_code != 200:
+            pass    # logging
+
+        bs = BeautifulSoup(r.text, 'html.parser')
+
+    def is_exists(self, collection, query):
+        self.mongo_client[self.db_name][collection].findOne(query)
+
+
+    def search_tweets(self, collection, query, store_db=False):
+        tweets = self.twitter_api.GetSearch(term=query, count=10000)
+        
+        for tweet in tweets:
+            t_t = dict()
+            t_t['id'] = tweet.id
+            t_t['user'] = tweet.user.name
+            t_t['profile_image_url'] = tweet.user.profile_image_url
+            t_t['text'] = self.get_tweet_full_content(tweet.text) if tweet.truncated else tweet.text
+
+            if store_db:
+                if self.is_exists(collection, {'id': tweet.id}):
+                    continue
+                self.mongo_client[self.db_name][collection].insert_one(t_t)
+            self.pprinter.pprint(t_t)
+
+
+
+'''
+account = '@meta_kongz'
+print(twitter_api.GetUserTimeline(screen_name=account, count=100, include_rts=False, exclude_replies=False))
+
+def search_minting(query):
+    query = '민팅 klay -nftart -filter:links -filter:replies -filter:retweets'
+    #keyword_necesary = ['민팅']
+    tweets = twitter_api.GetSearch(term=query, count=10000)
+    #[print(tweets[i].text+'\n'+'-'*60) for i in range(len(tweets))]
+    for tweet in tweets:
+        print(f'id: {tweet.id}')
+        print(f'Project: {tweet.user.name}')
+        print(f'Agenda: {tweet.text}')
+        print('-'*40)
+    print(tweets[-2])
+
+query = ['nft', 'minting']
+
+stream = twitter_api.GetStreamFilter(track=query)
+while True:
+    for tweets in stream:
+        try:
+            print(tweets['text'])
+            print('-------------------------------------')
+
+        except Exception as e:
+            print(e)
+            break
+'''
