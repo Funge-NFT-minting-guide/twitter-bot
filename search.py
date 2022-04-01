3dec5facc552955d9356d60fa72c2a7f7e9155f4
vulpes0zedra <vulpes0zedra@gmail.com>
2022-04-01 22:50:15 +0900

    amends filter and collection name

--- search.py
+++ search.py
@@ -4,4 +4,4 @@ from twitty import *
 bot = TwitterBot('twitty', store_db=True)
 
 #bot.search_tweets('test', '민팅 klay', full=True)
-bot.search_tweets('minting', '민팅 klay -nftart -filter:links -filter:replies -filter:retweets')
+bot.search_tweets('minting_tweets', '민팅 klay -nftart -filter:links -filter:replies -filter:retweets')
--- stream.py
+++ stream.py
@@ -4,6 +4,6 @@ from twitty import *
 bot = TwitterBot('twitty', store_db=True)
 
 
-#bot.add_rules('민팅 klay -nftart -is:reply -is:retweet', "minting")
-#bot.add_rules('에어드랍 -nftart -is:reply -is:retweet', "airdrop")
+#bot.add_rules('민팅 klay -nftart -is:reply -is:retweet', "minting_tweets")
+#bot.add_rules('에어드랍 (WL OR  퍼블릭 OR KLAY OR 이벤트 OR NFT) -nftart -is:reply -is:retweet ', "airdrop_tweets")
 bot.stream_filter()
