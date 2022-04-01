c2eb6505c3b9cd022f6712e12e512fb60d0c6250
vulpes0zedra <vulpes0zedra@gmail.com>
2022-04-01 20:25:43 +0900

    seperates twitty-api and remote cacheed file

--- stream.py
+++
@@ -1,9 +0,0 @@
-from twitty import *
-
-
-bot = TwitterBot('twitty', store_db=True)
-
-
-#bot.add_rules('민팅 klay -nftart -is:reply -is:retweet', "minting")
-#bot.add_rules('에어드랍 -nftart -is:reply -is:retweet', "airdrop")
-bot.stream_filter()
